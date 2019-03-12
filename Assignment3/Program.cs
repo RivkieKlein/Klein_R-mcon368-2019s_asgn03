a68b902b005dcc0fc1524c2d6045ab938318703f
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2019-03-12 13:52:11 -0400

    almost done besides for question 5

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -10,6 +10,30 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
+            Sale[] sales;
+            
+            //question 1
+            var itemPriceOver10 =
+                from s in sales
+                where s.PricePerItem >= 10.0
+                select s;
+
+            
+
+            //question 2
+            var itemQuant1 =
+                from s in sales
+                where s.Quantity == 1
+                orderby s.PricePerItem descending
+                select s;
+
+            //question 3
+
+            //question 4
+            var itemTea =
+               from s in sales
+               where s.Item == "Tea" && !s.ExpeditedShipping
+               select s;
         }
     }
 }
